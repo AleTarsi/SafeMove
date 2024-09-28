bcccf67152a6f58e547a1f2d958305fd092e8aa5
TarsAle <alessandro.tarsi98@gmail.com>
2024-09-28 17:37:46 +0200

    adding the licence

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # SafeMove
+SafeMove is an open-source project that aims at assess ergonomic risk through the RULA standard using AI.
 
 ## Dependencies:
 
@@ -6,8 +7,22 @@
     ```bash
     python -m ensure pip --default-pip
     ```
-2. Install the following packages using pip:
+2. Install the following packages:
     ```bash
     pip install xlsxwriter pandas mediapipe opencv-python
     ```
+
+## Usage:
+Enter in the project directory and launch the following script:
+
+```bash
+python scripts/sm_01_main.py
+```
+
+## Results:
+Enter in the folder `output` to visualize the results of your test.
+
+## Licence:
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
--- scripts/sm_01_main.py
+++ scripts/sm_01_main.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) [YEAR] [Your Name or Your Organization]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import cv2
 import mediapipe as mp
 import datetime
@@ -29,11 +51,11 @@ assert Activity in range(0,4)
 current_folder = os.getcwd()
 video_folder ="/videos/"
 ################# Select video here #######################
-video = 'video_10'
+video = 'test_video'
 ###################################################################
 path = current_folder + video_folder + video + ".mp4"
 
-assert os.path.exists(path), f"Video does not exist in the specified path: {path}.mp4"
+assert os.path.exists(path), f"Video does not exist in the specified path: {path}"
 
 cap = cv2.VideoCapture(path) # 0 for webcam
 # cap = cv2.VideoCapture(0) # 0 for webcam
